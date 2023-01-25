7d5a539b0c956b1fbdc3dcb9ad50dfbeaa59069a
David <74567759+FreshCrispyBacon@users.noreply.github.com>
2023-01-25 19:13:01 +0100

    Update resizefinal.py

--- resizefinal.py
+++ resizefinal.py
@@ -1,17 +1,33 @@
-import numpy as np
-import pandas as pd
-from IPython.display import Image, display
-import glob
 import cv2
+from datetime import datetime
 
-lijst = []
-for filename in glob.glob("C:/Users/davka/Downloads/ads2/resized/*"):
-    lijst.append(filename)
-lijst = [[x[-28:-14],x[-14:-11],x[-5:-4]] for x in lijst]
+counter = 0
+now = datetime.now()
+print('Starting time: ', datetime.now())
 
-df = pd.DataFrame(lijst, columns=['Tijd','MS','Nummer'])
-df['Tijd'] = pd.to_datetime(df['Tijd'])
-df = df.sort_values(['Tijd','MS','Nummer'])
-df = df[df['Nummer']!='6']
-df = df.reset_index()
-print(df)
+for filename in glob.glob('alles/20221005_selectie_360_5/*'):
+    
+    # read image
+    img = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
+    #print('Original Dimensions of image number ', counter, ' is: ',img.shape)
+ 
+    # new dimensions
+    width = 640
+    height = 640
+    dim = (width, height)
+ 
+    # resize image
+    resized = cv2.resize(img, dim)
+ 
+    #print('Resized Dimensions : ',resized.shape)
+    
+    # save new image
+    if filename[-5:-4] == '1':
+        cv2.imwrite('resizedfinal/'+filename[-28:] ,resized)
+        print(filename[-28:])
+        print('Image number: ', counter)
+    
+        counter += 1
+
+print('Finished time: ', datetime.now())
+print('Total time: ', datetime.now()-now)
